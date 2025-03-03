1a249e33f03707cd896d49c2ce497073c8171878
Gabriel Kiyoto <120345849+Nagi1867@users.noreply.github.com>
2025-03-03 16:42:55 -0300

    Conectando ao MongoDB com repository e service

--- src/main/java/com/course/resources/UserResource.java
+++ src/main/java/com/course/resources/UserResource.java
@@ -1,6 +1,8 @@
 package com.course.resources;
 
 import com.course.domain.User;
+import com.course.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,13 +15,12 @@ import java.util.List;
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
+    @Autowired
+    private UserService service;
 
     @GetMapping
     public ResponseEntity<List<User>> findAll() {
-        User maria = new User("1", "maria", "maria@gmail.com");
-        User alex = new User("2", "alex", "alex@gmail.com");
-        List<User> list = new ArrayList<>();
-        list.addAll(Arrays.asList(maria, alex));
+        List<User> list = service.findAll();
         return ResponseEntity.ok().body(list);
     }
 }
