07c23a0f899e1ff259643a4bf19898401a5f25d0
Gabriel Kiyoto <120345849+Nagi1867@users.noreply.github.com>
2025-03-06 17:33:43 -0300

    Obtendo post por id

---
+++ src/main/java/com/course/services/PostService.java
@@ -0,0 +1,25 @@
+package com.course.services;
+
+import com.course.domain.Post;
+import com.course.domain.User;
+import com.course.repository.PostRepository;
+import com.course.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PostService {
+    @Autowired
+    private PostRepository repository;
+
+    public Post findById(String id) {
+        Optional<Post> user = repository.findById(id);
+        if (user.isEmpty()) {
+            throw new ObjectNotFoundException("Objeto não encontrado!");
+        }
+        return user.get();
+    }
+}
